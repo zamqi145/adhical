d1cf2eb681c8abbe6f3f35bd74c1cdeaa7091800
Aeknasd145 <abbasjames91@gmail.com>
2019-04-18 22:56:25 +0300

    Create sifremi-unuttum.js

---
+++ sifremi-unuttum.js
@@ -0,0 +1,1 @@
+$(function(){$("#btn").click(function(){var veri=$("#frm").serialize();$.ajax({type:"post",url:"islem/sifremi-unuttum.php",data:veri,success:function(sonuc){$("#sonuc").html((sonuc));}});});});

21ba836b471c465d5b6059f58df0fd87853f0abe
Aeknasd145 <abbasjames91@gmail.com>
2019-04-18 22:32:24 +0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,38 @@
+<?php 
+  if(!defined("FROM_FILE")) { 
+    exit(); 
+  } 
+?>
+<footer class="col-md-12 col-sm-12 col-xs-12 col-lg-12 footer-big">
+  <div class="col-md-4 col-sm-12 col-xs-12 col-lg-4">
+    <p class="p1">Destek Ekibi</p>
+    <ul class="ul-1">
+      <li class="li-2"><a href="iletisim">İletişim</a></li>
+    </ul>
+  </div>
+  <div class="col-md-3 col-sm-12 col-xs-12 col-lg-3">
+    <p class="p1">Hakkımızda</p>
+    <ul class="ul-3">
+      <li class="li-5"><a href="hakkimizda">Hakkımızda</a></li>
+      <li class="li-5"><a href="gizlilik">Gizlilik</a></li>
+    </ul>
+  </div>
+  <div class="col-md-4 col-sm-12 col-xs-12 col-lg-4">
+    <p class="p1">Sosyal Medya Hesaplarımız</p>
+    <ul class="ul-2">
+      <li class="li-3"><a href="https://www.fb.com/"><i class="fab fa-facebook-square facebook"></i></a></li>
+      <li class="li-4"><a href="https://www.instagram.com/"><i class="fab fa-instagram instagram"></i></a></li>
+    </ul>
+  </div>
+</footer>
+<link rel="stylesheet" type="text/css" href="/css/style.css">
+<link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+<script src="https://www.website.com/reklam728-90.js"></script>
+<script src="https://www.website.com/reklam300-600.js"></script>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+<script src="/js/jquery.min.js" type="text/javascript"></script>
+<script src="/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="https://code.jquery.com/jquery-2.0.3.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="/giris.js"></script>
+</body>
+</html>

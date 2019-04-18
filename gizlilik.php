ff68d2c9ace8717bad004aae63b3871bb8fd3f47
Aeknasd145 <abbasjames91@gmail.com>
2019-04-18 22:36:31 +0300

    Create gizlilik.php

---
+++ gizlilik.php
@@ -0,0 +1,17 @@
+<?php 
+  include("connect.php"); 
+  $titl = "Your Title"; 
+  $desc = "Your Description"; 
+  $keyw = "Your Keywords"; 
+  define("FROM_FILE", "1"); 
+  include("header.php"); 
+?>
+<div style="text-align: center;">
+  <div class="col-sm-12">
+    <h2 style="color: black;">Gizlilik Politikası - Website Name Gizlilik Politikası</h2>
+    <p style="color: black;">Bu gizlilik politikası ile website.com internet sitesi (Gizlilik politikasında "website.com" olarak anılacaktır) üzerinden kullanıcıların website.com'da bulunan kişisel bilgilerinin kullanımı ve korunması düzenlenmektedir.<br /><br />Kullanıcıların adı – soyadı, telefon numarası, e-posta adresi, internet sitesi adresi gibi kullanıcıyı doğrudan ya da dolaylı olarak tanımlamaya yönelik her türlü bilgi, bu gizlilik politikasında “kişisel bilgiler” olarak anılacaktır.<br /><br />website.com kullanıcılarının kişisel bilgileri, bu gizlilik politikası ile belirlenen esas ve kapsam haricinde hiçbir şekilde kullanılmayacak ve ancak kullanıcının açık rızası ile üçüncü kişilerle paylaşılacaktır.<br /><br />website.com, kişisel bilgilerin ve website.com kullanıcılarının gerçekleştirmiş olduğu işlem bilgilerinin güvenliğini ve gizliliğini sağlamak için gerekli önlemleri almaktadır. website.com bu amaçla, başka programlar kullanabilir. Ancak, website.com tarafından gerekli bilgi güvenliği önlemlerinin almasına karşın internet sitesine ve / veya sisteme yapılan saldırılar sonucunda kişisel bilgilerin zarar görmesi veya üçüncü kişilerin eline geçmesi durumunda, website.com'un bu nedenle herhangi bir sorumluluğu olmayacaktır.<br /><br />Bir kullanıcının, bir başka kullanıcının bilgilerine ulaşması ve bu bilgileri değiştirmesi mümkün değildir. Kullanıcının kişisel bilgileri, website.com haricinde yalnızca kullanıcı tarafından ulaşılabilir ve bu bilgiler ancak kullanıcı tarafından güncellenebilir. Kullanıcının talebi ile kişisel bilgiler website.com tarafından da güncellenebilecektir.<br /><br />website.com, kişisel bilgileri ölçümleme çalışmaları, reklam, tanıtım, pazarlama ile sair iletişim faaliyetleri amacıyla kullanabileceği ve Grup Şirketleri ile iş ortaklarıyla paylaşabileceği gibi website.com tarafından sunulan hizmet kalitesinin arttırılması ve site faaliyetlerinin daha iyi şekilde yürütülmesi amacıyla da kayıt altına alınabilir ve kullanabilir.<br /><br />İdari kurumlar ve adli kurumlar tarafından gelen talepler ve bildirimler doğrultusunda kişisel bilgiler, Gizlilik Politikasına uygun olarak adli ve idari kurumlar ile paylaşılabilir.<br /><br />Kullanıcının kişisel bilgileri; gerçekleştirilen işlemlerde gerekli görülmesi halinde, sürecin hızlandırılması ve kullanıcılar arasındaki iletişimi sağlamak amacıyla işleme dahil olan diğer kullanıcı ile paylaşılabilir.<br /><br />website.com internet sitesini ziyaret eden her kullanıcı, website.com gizlilik politikası hükümlerini kabul etmiş sayılacaktır.<p><br />
+  </div>
+</div>
+<?php 
+  include("footer.php"); 
+?>
